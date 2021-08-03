ba6f3eb83ccd8f86a38337bac81a54ba4c8dd16a
Jordan Ubaldo <ubaldojordan@gmail.com>
2021-08-03 16:01:02 -0400

    Pushing for Michael

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -19,7 +19,8 @@ if(currentToken != null) {
 export default new Vuex.Store({
   state: {
     token: currentToken || '',
-    user: currentUser || {}
+    user: currentUser || {},
+    games: []
   },
   mutations: {
     SET_AUTH_TOKEN(state, token) {
@@ -37,6 +38,9 @@ export default new Vuex.Store({
       state.token = '';
       state.user = {};
       axios.defaults.headers.common = {};
+    },
+    SET_GAMES(state, data) {
+      state.games = data;
     }
   }
 })
