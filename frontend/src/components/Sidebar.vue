7e5057722150b225ead5e751e55615ff864626ac
Michael Buckner <bucknerml@gmail.com>
2021-08-03 16:02:13 -0400

    Added Sidebar.vue component.

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <game-list />
+      <invite-list />
+      SIDEBAR
+  </div>
+</template>
+
+<script>
+import GameList from '@/components/GameList.vue';
+import InviteList from '@/components/InviteList.vue';
+import Login from '@/components/Login.vue';
+import Register from '@/components/Register.vue';
+
+export default {
+    name: "TheSidebar",
+    components: {
+        GameList,
+        InviteList,
+        Login,
+        Register
+    }
+};
+</script>
+
+<style>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import Test from '@/views/Test.vue'
 
 Vue.use(Router)
 
@@ -53,6 +54,11 @@ const router = new Router({
         requiresAuth: false
       }
     },
+    {
+      path: "/test",
+      name: "test",
+      component: Test
+    }
   ]
 })
 
