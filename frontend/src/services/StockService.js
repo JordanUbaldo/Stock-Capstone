c2e1930da0d95e648b0aea4f8cd02aede41fef32
Michael Buckner <bucknerml@gmail.com>
2021-08-05 15:46:00 -0400

    Added method to StockService to post a trade to database. Renamed axios instances to differentiate between external and internal API calls.

--- frontend/src/services/StockService.js
+++ frontend/src/services/StockService.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
 const key = "/quote?token=Tpk_f2f602e084b44aa5a811ffd1445bc357"
-const http = axios.create({
+const httpExternal = axios.create({
     baseURL: "https://sandbox.iexapis.com/stable/stock/"
 });
+
+const httpInternal = axios.create({
+    baseURL: "http://localhost:8080"
+})
 
 export default {
 
     getStock(ticker) {
-        return http.get(`/${ticker}${key}`);
+        return httpExternal.get(`/${ticker}${key}`);
+    },
+    postTrade(trade, token) {
+        return httpInternal.post(trade, {headers: {'Authorization': `Bearer ${token}`}})
     }
 }
