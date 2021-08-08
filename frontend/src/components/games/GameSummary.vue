690e5316880cc47b2c561f533de5a9eabb478ffb
Michael Buckner <bucknerml@gmail.com>
2021-08-08 12:26:16 -0400

    Changed directory structure of frontend components.

---
+++ frontend/src/components/games/GameSummary.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <!-- Game Name -->
+      <p>Game: {{ $store.state.currentGameName }}</p>
+      <!-- End Date -->
+      <p>End Date: {{ (new Date($store.state.currentGameEndDate)).toLocaleString() }}</p>
+      <!-- Invite Button for Host -->
+      <button type="button" @click='routeToInvite'>Invite</button>
+  </div>
+</template>
+
+<script>
+import userService from '@/services/UserService.js';
+
+export default {
+    name: "game-summary",
+
+    methods: {
+      routeToInvite() {
+        userService.getUsersForGame(this.$store.state.currentGameId, this.$store.state.token)
+        .then(response => {
+            if (response.status === 200) {
+                this.$store.commit("SET_CURRENT_GAME_USERS", response.data)
+            }
+        });
+        this.$router.push({ name: 'invite', params: { gameId : this.$store.state.currentGameId}})
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/games/LeaderBoard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Leaderboard</h2>
+    <p>{{ leaderboard }}</p>
+  </div> 
+</template>
+
+<script>
+import gamesService from '@/services/GamesService'
+export default {
+    name: "leader-board",
+    data() {
+      return {
+        leaderboard: {}
+      }
+    },
+    async created() {
+      let response = await gamesService.getLeaderboard(this.$store.state.currentGameId, this.$store.state.token);
+      this.leaderboard = response.data;
+    }
+}
+</script>
+
+<style>
+
+</style>
