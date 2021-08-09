3986bdb5599cf76ad70c3ef9c3d86056e8dbc30c
Michael Buckner <bucknerml@gmail.com>
2021-08-09 15:52:05 -0400

    Possible header implementation.

---
+++ frontend/src/views/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="appTitle">
+      <p>  Title of Application
+          </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TheTitle'
+}
+</script>
+
+<style>
+    #appTitle {
+        font-size: x-large;
+        color: red;
+    }
+
+</style>
